b94539008cb20a6cd7363765b53bdeb49380ae08
Aaron D. Schroeder <dads2busy@gmail.com>
2017-12-16 17:27:31 +0000

    return type choice and chaining data.table

--- R/name.R
+++ R/name.R
@@ -3,6 +3,7 @@
 #' @param df A data.frame or data.table with a suffix containing name column.
 #' @param name_field Name of the name column/field.
 #' @param suffix_list A list of suffixes to move.
+#' @param return_type Type of data object to return, "dt" (data.table) or "df" (data.frame). Default "dt".
 #' @import data.table
 #' @export
 #' @examples
@@ -13,12 +14,18 @@
 #' 1  Bob,     Jr
 #' 2  Dallas    III
 #' 3  Willy   <NA>
-move_suffix <- function(df, name_field, suffix_list) {
+move_suffix <- function(df, name_field, suffix_list, return_type = "dt") {
     dt <- data.table::setDT(df)
     lapply(suffix_list, function(sfx) {
         field <- dt[, get(name_field)]
-        dt[field %like% paste0(" ", sfx), suffix := sfx]
-        dt[, eval(name_field):=sub(paste0(" ", sfx), "", field)]
+        dt[
+            field %like% paste0(" ", sfx),
+            suffix := sfx
+        ][
+            ,
+            eval(name_field):=sub(paste0(" ", sfx), "", field)
+        ]
     })
-    data.table::setDF(dt)
+    ifelse(return_type == "df", d <- data.table::setDF(dt), d <- dt)
+    d
 }
