e265c54c7e746415ea150975c0d624ea8cc6baab
Aaron D. Schroeder <dads2busy@gmail.com>
2018-03-06 20:52:35 +0000

    new file functions

---
+++ R/file.R
@@ -0,0 +1,28 @@
+#' Unzip File to Temp Directory and Return Directory Path
+#'
+#' @param zipfilepath Path to the zip file.
+#' @export
+#' @examples
+#' unzip2temp("data/CENSUS/cb/cb_2016_01_bg_500k.zip")
+#' [1] "/tmp/Rtmp8lE8Pj/"
+file.unzip2temp <- function(zipfilepath) {
+    tempdir <- tempdir()
+    print(unzip(zipfilepath, exdir = tempdir))
+    tempdir
+}
+
+#' Find By File Type in Directory
+#'
+#' @param type Type of file file extension without a period, e.g. "zip", "shp".
+#' @param first_only Only return first found. default FALSE.
+#' @param full_path Return file name with full path. defualt FASLE.
+#' @export
+#' @examples
+#' findByType(tmpdir, "shp", first_only = TRUE)
+#' [1] "some_shape_file.shp"
+file.findByType <- function(directory, type = "shp", first_only = FALSE, full_path = FALSE) {
+    if (first_only == FALSE) list.files(directory, pattern = paste0(".*\\.", type, "$"), full.names = full_path)
+    else list.files(directory, pattern = paste0(".*\\.", type, "$"), full.names = full_path)[[1]]
+}
+
+
