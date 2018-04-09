c16715da976b8bfe9d4a8d3c3c169e8f64e4706d
Aaron D. Schroeder <dads2busy@gmail.com>
2018-04-09 22:02:10 +0000

    rename

--- R/file.R
+++ R/file.R
@@ -16,12 +16,27 @@ file.unzip2temp <- function(zipfilepath) {
 #' @param url url to download the zip file.
 #' @export
 file.download_unzip2temp <- function(url) {
+    browser()
     tempdir <- tempdir()
     file.remove(list.files(tempdir, full.names = T))
     tempfile <- file.path(tempdir, basename(url))
     download.file(url, tempfile)
     utils::unzip(tempfile, exdir = tempdir)
     file.remove(tempfile)
+    list.files(tempdir, full.names = T)
+}
+
+#' Download and Un gzip File to Temp Directory and Return File Paths
+#'
+#' @param url url to download the zip file.
+#' @export
+file.download_ungz2temp <- function(url) {
+    browser()
+    tempdir <- tempdir()
+    file.remove(list.files(tempdir, full.names = T))
+    tempfile <- file.path(tempdir, basename(url))
+    download.file(url, tempfile)
+    R.utils::gunzip(tempfile)
     list.files(tempdir, full.names = T)
 }
 
