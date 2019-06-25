c8e63c06517635d221ec70485436b2f21b88db31
Aaron D. Schroeder <dads2busy@gmail.com>
2019-06-25 19:17:11 +0000

    try_try_try

---
+++ R/try.R
@@ -0,0 +1,15 @@
+#' Try running a function and get a non-error response n times
+#'
+#' @param x A function.
+#' @export
+#' @examples
+try_try_try <- function(x, n = 3L) {
+  response <- "failed"
+  attempt <- 1
+  while (response == "failed" && attempt <= n) {
+    print(sprintf("attempt: %s", attempt))
+    attempt <- attempt + 1
+    try({ response <- x }, silent = TRUE)
+  }
+  response
+}
